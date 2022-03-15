6b9cd8e46af7ae7e4e813916d2da4e7a2d0c5aa4
dunmi9791 <dominic.anyanna@gmail.com>
2022-03-15 09:55:33 +0100

    store released

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'om_account_budget', 'accounting_pdf_reports', 'tis_tally_bs_and_pl'],
+    'depends': ['base', 'account', 'om_account_budget', 'accounting_pdf_reports', 'tis_tally_bs_and_pl', 'budget_releases'],
 
     # always loaded
     'data': [
